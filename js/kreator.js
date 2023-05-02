d5d2376e051a99eec7ac8b74a5b13028f9e56df5
PieselKlif <pieselklif@gmail.com>
2023-05-02 23:09:02 +0200

    fix buttons and start js

--- js/kreator.js
+++ js/kreator.js
@@ -5,3 +5,13 @@ numberInput.addEventListener("input", () => {
     numberInput.value = numberInput.value.slice(0, 3);
   }
 });
+
+function close_sekcja(sekcja_id) {
+  var sekcja = document.getElementById('sekcja_' + sekcja_id);
+  sekcja.remove()
+}
+
+function close_zawartosc(sekcja_id , zawartosc_id) {
+  var zawartosc = document.getElementById('zawartosc_' + sekcja_id + '_' + zawartosc_id);
+  zawartosc.remove()
+}
